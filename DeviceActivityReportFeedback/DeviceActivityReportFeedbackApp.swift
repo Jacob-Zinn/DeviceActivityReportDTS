7bcc484d8290ebcad8b80e4ecba8ba18c2b9c414
Jacob Zinn <jacobpzinn@gmail.com>
2024-12-30 15:16:22 -0800

    Initial Commit

---
+++ DeviceActivityReportFeedback/DeviceActivityReportFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceActivityReportFeedbackApp.swift
+//  DeviceActivityReportFeedback
+//
+//  Created by Jacob Zinn on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DeviceActivityReportFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
